d9f785a11a830971a201987d5bb3b00f7e327392
David Schroeder <david.schroeder@hp.com>
2014-05-22 14:37:40 -0600

    JAH-211 forwarder bug fix, JAH-222 metric rename

--- monagent/collector/checks_d/http_check.py
+++ monagent/collector/checks_d/http_check.py
@@ -47,31 +47,31 @@ class HTTPCheck(AgentCheck):
         except socket.timeout, e:
             length = int((time.time() - start) * 1000)
             self.log.info("%s is DOWN, error: %s. Connection failed after %s ms" % (addr, str(e), length))
-            self.gauge('mon_http_status', 1, dimensions=new_dimensions)
+            self.gauge('http_status', 1, dimensions=new_dimensions)
             return
 
         except HttpLib2Error, e:
             length = int((time.time() - start) * 1000)
             self.log.info("%s is DOWN, error: %s. Connection failed after %s ms" % (addr, str(e), length))
-            self.gauge('mon_http_status', 1, dimensions=new_dimensions)
+            self.gauge('http_status', 1, dimensions=new_dimensions)
             return
 
         except socket.error, e:
             length = int((time.time() - start) * 1000)
             self.log.info("%s is DOWN, error: %s. Connection failed after %s ms" % (addr, repr(e), length))
-            self.gauge('mon_http_status', 1, dimensions=new_dimensions)
+            self.gauge('http_status', 1, dimensions=new_dimensions)
             return
 
         except Exception, e:
             length = int((time.time() - start) * 1000)
             self.log.error("Unhandled exception %s. Connection failed after %s ms" % (str(e), length))
-            self.gauge('mon_http_status', 1, dimensions=new_dimensions)
+            self.gauge('http_status', 1, dimensions=new_dimensions)
             raise
 
         if response_time:
             # Stop the timer as early as possible
             running_time = time.time() - start
-            self.gauge('mon_http_response_time', running_time, dimensions=new_dimensions)
+            self.gauge('http_response_time', running_time, dimensions=new_dimensions)
 
         # Add a 'detail' tag if requested
         if include_content:
@@ -79,16 +79,16 @@ class HTTPCheck(AgentCheck):
 
         if int(resp.status) >= 400:
             self.log.info("%s is DOWN, error code: %s" % (addr, str(resp.status)))
-            self.gauge('mon_http_status', 1, dimensions=new_dimensions)
+            self.gauge('http_status', 1, dimensions=new_dimensions)
 
         if pattern is not None:
             if re.search(pattern, content, re.DOTALL):
                 self.log.debug("Pattern match successful")
             else:
                 self.log.info("Pattern match failed! '%s' not in '%s'" % (pattern, content))
-                self.gauge('mon_http_status', 1, dimensions=new_dimensions)
+                self.gauge('http_status', 1, dimensions=new_dimensions)
                 return
 
         self.log.debug("%s is UP" % addr)
-        self.gauge('mon_http_status', 0, dimensions=new_dimensions)
+        self.gauge('http_status', 0, dimensions=new_dimensions)
 
