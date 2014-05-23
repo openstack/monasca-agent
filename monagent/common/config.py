ce5d0c69185570b1a4f1da4dcf6307180f4744dd
gary-hessler <gary.hessler@hp.com>
2014-05-23 14:58:38 -0600

    Moving version information to central location

--- monagent/common/config.py
+++ monagent/common/config.py
@@ -13,6 +13,7 @@ import re
 import imp
 from optparse import OptionParser, Values
 from cStringIO import StringIO
+from version import __version__
 
 import yaml
 
@@ -56,7 +57,7 @@ def get_parsed_args():
 
 
 def get_version():
-    return "1.0.3"
+    return __version__
 
 
 def skip_leading_wsp(f):
