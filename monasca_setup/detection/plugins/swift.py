02343861a3855273884b87b2cd772d3c3cf4c37b
Keshava Bharadwaj <keshava@hp.com>
2015-05-28 23:11:39 +0530

    Refactors swift metrics into separate check
    This patch separates out swift metrics into separate checks. Earlier
    all the swift metrics which were wrapped around icinga were part
    of service plugin. With this, the status and messages of each diagnostic
    was part of value_meta. This causes shortcoming since alarm definition
    cannot be based on value_meta.
    
    With this change, all the swift metrics are now part of separate check,
    which each of values gauged separately. The services are still part of
    detection plugin.
    
    Change-Id: I53934858c07450bce33290e48adfde65949bd0c4

--- monasca_setup/detection/plugins/swift.py
+++ monasca_setup/detection/plugins/swift.py
@@ -1,39 +1,4 @@
-import os
-
 import monasca_setup.detection
-
-DIAG_COMMAND = '/usr/local/bin/diagnostics'
-CHECKER_COMMAND = '/usr/local/bin/swift-checker'
-
-diag_attributes = ['check_mounts', 'disk_monitoring', 'file_ownership',
-                   'network_interface', 'ping_hosts', 'drive_audit']
-
-checker_attributes = ['diskusage', 'healthcheck', 'replication']
-
-
-def get_instances(command, attributes):
-    """
-    Fetch instances per command type and attribute
-    """
-    instances = []
-    for attribute in attributes:
-        cmd = dict()
-        cmd['name'] = '{0}.{1}'.format('swift', attribute)
-        cmd['check_command'] = '{0} --{1}'.format(command, attribute)
-        cmd['check_interval'] = 60
-        cmd['dimensions'] = {'service': 'swift'}
-        instances.append(cmd)
-    return instances
-
-
-def get_config_instances():
-    """
-    Fetch all instances per command type
-    """
-    config_instances = []
-    config_instances.extend(get_instances(DIAG_COMMAND, diag_attributes))
-    config_instances.extend(get_instances(CHECKER_COMMAND, checker_attributes))
-    return config_instances
 
 
 class Swift(monasca_setup.detection.ServicePlugin):
@@ -42,7 +7,7 @@ class Swift(monasca_setup.detection.ServicePlugin):
 
     """
 
-    def __init__(self, template_dir, overwrite=True, args=None):
+    def __init__(self, template_dir, overwrite=True):
         service_params = {
             'template_dir': template_dir,
             'overwrite': overwrite,
@@ -59,14 +24,3 @@ class Swift(monasca_setup.detection.ServicePlugin):
         }
 
         super(Swift, self).__init__(service_params)
-
-    def build_config(self):
-        config = super(Swift, self).build_config()
-
-        if os.path.exists(DIAG_COMMAND) and \
-                os.path.exists(CHECKER_COMMAND):
-            config['nagios_wrapper'] = {'init_config': None,
-                                        'instances': get_config_instances(),
-                                        }
-
-        return config
