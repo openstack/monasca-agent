cc239df503f909955228c3bae81326f6f0f47be4
Juerg Haefliger <juerg.haefliger@hp.com>
2015-07-01 11:35:39 +0200

    Add a detection plugin for the crash check plugin
    Simply check if a crash kernel is loaded to determine if the crash check
    plugin should be enabled. If enabled, it is currently assumed, that crash
    dumps are stored under /var/crash.
    
    Change-Id: If17489c76569c4239378b16e2a75c080937b3ced
    Signed-off-by: Juerg Haefliger <juerg.haefliger@hp.com>

---
+++ monasca_setup/detection/plugins/crash.py
@@ -0,0 +1,41 @@
+import logging
+import os
+
+from monasca_setup import agent_config
+from monasca_setup.detection import Plugin
+
+log = logging.getLogger(__name__)
+
+
+class Crash(Plugin):
+    """
+    Detect if kdump is installed and enabled and setup configuration to
+    monitor for crash dumps.
+    """
+
+    def _detect(self):
+        """
+        Run detection, set self.available True if a crash kernel is loaded.
+        """
+        loaded = '/sys/kernel/kexec_crash_loaded'
+        if os.path.isfile(loaded):
+            with open(loaded, 'r') as fh:
+                if fh.read().strip() == '1':
+                    self.available = True
+
+    def build_config(self):
+        """
+        Build the config as a Plugin object and return it.
+        """
+        log.info('\tEnabling the Monasca crash dump healthcheck')
+        config = agent_config.Plugins()
+
+        config['crash'] = {
+            'init_config': None,
+            'instances': [{'name': 'crash_stats'}]
+        }
+
+        return config
+
+    def dependencies_installed(self):
+        return True
