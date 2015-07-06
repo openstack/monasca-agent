b7bc1524b80603d2d37c43387916236ed26b8e08
d042680 <root@mo-79e9e5fcb.mo.sap.corp>
2015-07-06 16:30:09 +0000

    add support for (user-) domain_id/name

--- monasca_agent/common/keystone.py
+++ monasca_agent/common/keystone.py
@@ -24,12 +24,14 @@ class Keystone(object):
         auth_url = self.config.get('keystone_url', None)
         username = self.config.get('username', None)
         password = self.config.get('password', None)
+	domain_id = self.config.get('domain_id', None)
+	domain_name = self.config.get('domain_name', None)
         insecure = self.config.get('insecure', False)
         cacert = self.config.get('ca_file', None)
         project_id = self.config.get('project_id', None)
         project_name = self.config.get('project_name', None)
-        project_domain_name = self.config.get('project_domain_name', None)
-        project_domain_id = self.config.get('project_domain_id', None)
+        project_domain_name = self.config.get('domain_name', None)
+        project_domain_id = self.config.get('domain_id', None)
 
         kc_args = {'auth_url': auth_url,
                    'username': username,
@@ -48,6 +50,13 @@ class Keystone(object):
                 kc_args.update({'domain_name': project_domain_name})
             if project_domain_id:
                 kc_args.update({'domain_id': project_domain_id})
+	elif domain_id:
+                kc_args.update({'user_domain_id': domain_id})
+	elif domain_name:
+                kc_args.update({'user_domain_name': domain_name})
+
+        # TO BE REMOVED BY JOACHIM
+        log.info("Creating the Keystone Client for {0}".format(repr(kc_args))) 
 
         return ksclient.KSClient(**kc_args)
 
