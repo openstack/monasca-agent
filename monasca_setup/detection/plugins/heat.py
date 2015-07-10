b3282c188219dc62a2c66e906ec1e05c6babf9e9
tyagi <ishant.tyagi@hp.com>
2015-07-10 03:20:36 -0700

    Add heat service detection plugin
    This patch will enable heat processes to be monitored
    by monasca.
    
    Change-Id: Ib09ae206f5f1a937ff3995fba5ac4efdbd0b4973

---
+++ monasca_setup/detection/plugins/heat.py
@@ -0,0 +1,20 @@
+import monasca_setup.detection
+
+
+class Heat(monasca_setup.detection.ServicePlugin):
+
+    """Detect Heat daemons and setup configuration to monitor them."""
+
+    def __init__(self, template_dir, overwrite=True, args=None):
+        service_params = {
+            'args': args,
+            'template_dir': template_dir,
+            'overwrite': overwrite,
+            'service_name': 'orchestration',
+            'process_names': ['heat-api', 'heat-api-cfn',
+                              'heat-api-cloudwatch', 'heat-engine'],
+            'service_api_url': 'http://localhost:8004',
+            'search_pattern' : '.*200 OK.*',
+        }
+
+        super(Heat, self).__init__(service_params)
