182683df68af60ad623b57223c07f3436071da73
Steve Leon <kokhang@gmail.com>
2015-08-06 15:37:26 -0700

    Adding detection plugin for Cue
    Change-Id: I82e5829da3aacc899c8550672f9726238c1d1cdd

---
+++ monasca_setup/detection/plugins/cue.py
@@ -0,0 +1,19 @@
+import monasca_setup.detection
+
+
+class Cue(monasca_setup.detection.ServicePlugin):
+
+    """Detect Cue daemons and setup configuration to monitor them."""
+
+    def __init__(self, template_dir, overwrite=True, args=None):
+        service_params = {
+            'args': args,
+            'template_dir': template_dir,
+            'overwrite': overwrite,
+            'service_name': 'message-broker',
+            'process_names': ['cue-api', 'cue-worker'],
+            'service_api_url': 'http://localhost:8795/v1',
+            'search_pattern': '.*v1.*'
+        }
+
+        super(Cue, self).__init__(service_params)
