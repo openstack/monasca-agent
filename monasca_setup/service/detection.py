99e09f2952ebb1280671e8a516b4df14fe962c8f
Lukasz Zajaczkowski <lukasz.zajaczkowski@ts.fujitsu.com>
2015-09-15 13:21:55 +0000

    Centos and RedHat added as supported distributions
    Change-Id: I21ac0287b22e9ad125842e50205f45243d0a3b2d

--- monasca_setup/service/detection.py
+++ monasca_setup/service/detection.py
@@ -17,7 +17,7 @@ def detect_init(*args, **kwargs):
     """
     detected_os = platform.system()
     if detected_os == 'Linux':
-        supported_linux_flavors = ['ubuntu', 'debian']
+        supported_linux_flavors = ['ubuntu', 'debian', 'centos linux', 'red hat enterprise linux server']
         flavor = platform.linux_distribution()[0]
         if flavor.lower() not in supported_linux_flavors:
             log.warn('{0} is not a support Linux distribution'.format(flavor))
