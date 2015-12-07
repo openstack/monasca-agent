1a11dc3c8292212b9a0d5c8b13e0593fc1d538e7
Joachim Barheine <joachim.barheine@sap.com>
2015-12-07 15:57:40 +0100

    eliminate copy and paste and extreme cycl. complexity

--- monasca_agent/collector/checks_d/influxdb.py
+++ monasca_agent/collector/checks_d/influxdb.py
@@ -7,6 +7,7 @@ from httplib2 import Http
 from httplib2 import httplib
 from httplib2 import HttpLib2Error
 
+import json
 import logging
 import monasca_agent.collector.checks.services_checks as services_checks
 
@@ -106,18 +107,7 @@ class InfluxDB(services_checks.ServicesCheck):
                 length = int((time.time() - start) * 1000)
                 error_string = '{0} is DOWN, error: {1}. Connection failed ' \
                                'after {2} ms'.format(addr, str(e), length)
-                self.log.info(error_string)
-                if 'http_status' in whitelist:
-                    if http_status_mtype == GAUGE:
-                        self.gauge(http_status_mname,
-                                   1,
-                                   dimensions=dimensions,
-                                   value_meta={'error': error_string})
-                    elif http_status_mtype == RATE:
-                        self.rate(http_status_mname,
-                                  1,
-                                  dimensions=dimensions,
-                                  value_meta={'error': error_string})
+                self.push_error(error_string, dimensions, http_status_mname, http_status_mtype, whitelist)
 
                 return services_checks.Status.DOWN, error_string
 
@@ -127,18 +117,7 @@ class InfluxDB(services_checks.ServicesCheck):
                                'routable after {2} ms'.format(addr,
                                                               repr(e),
                                                               length)
-                self.log.info(error_string)
-                if 'http_status' in whitelist:
-                    if http_status_mtype == GAUGE:
-                            self.gauge(http_status_mname,
-                                       1,
-                                       dimensions=dimensions,
-                                       value_meta={'error': error_string})
-                    elif http_status_mtype == RATE:
-                            self.rate(http_status_mname,
-                                      1,
-                                      dimensions=dimensions,
-                                      value_meta={'error': error_string})
+                self.push_error(error_string, dimensions, http_status_mname, http_status_mtype, whitelist)
 
                 return services_checks.Status.DOWN, error_string
 
@@ -177,19 +156,7 @@ class InfluxDB(services_checks.ServicesCheck):
             if int(resp.status) >= 400:
                 error_string = '{0} is DOWN, error code: {1}'\
                     .format(addr, str(resp.status))
-                self.log.info(error_string)
-
-                if 'http_status' in whitelist:
-                    if http_status_mtype == GAUGE:
-                        self.gauge(http_status_mname,
-                                   1,
-                                   dimensions=dimensions,
-                                   value_meta={'error': error_string})
-                    elif http_status_mtype == RATE:
-                        self.rate(http_status_mname,
-                                  1,
-                                  dimensions=dimensions,
-                                  value_meta={'error': error_string})
+                self.push_error(error_string, dimensions, http_status_mname, http_status_mtype, whitelist)
 
                 return services_checks.Status.DOWN, error_string
 
@@ -214,9 +181,22 @@ class InfluxDB(services_checks.ServicesCheck):
             done = True
             return services_checks.Status.UP, success_string
 
+    def push_error(self, error_string, dimensions, http_status_mname, http_status_mtype, whitelist):
+        self.log.info(error_string)
+        if 'http_status' in whitelist:
+            if http_status_mtype == GAUGE:
+                self.gauge(http_status_mname,
+                           1,
+                           dimensions=dimensions,
+                           value_meta={'error': error_string})
+            elif http_status_mtype == RATE:
+                self.rate(http_status_mname,
+                          1,
+                          dimensions=dimensions,
+                          value_meta={'error': error_string})
+
     def _rate_or_gauge_statuses(self, content, dimensions,
                                 whitelist, metricmap):
-        import json
         statuses = {}
         measurements = {}
 
@@ -224,40 +204,7 @@ class InfluxDB(services_checks.ServicesCheck):
         self.log.debug("data %s" % (data))
         if data and 'results' in data and 'series' in data['results'][0]:
             for d in data['results'][0]['series']:
-                if 'name' in d:
-                    name = d['name']
-                    if 'server' in name:
-                        columns = d['columns']
-                        values = d['values'][0]
-
-                        # Just make sure we have the same amount of
-                        # data in both lists
-                        if len(columns) == len(values):
-                            cnt = 0
-                            while cnt < len(columns):
-                                cname = columns[cnt]
-                                cval = values[cnt]
-                                # See if we need to include the metric
-                                if cname in whitelist:
-                                    # See if we need to map it to the
-                                    # preferred name
-                                    if cname in metricmap:
-                                        mname, mtype = tuple(metricmap[cname])
-                                    # Otherwise just stuff in the original
-                                    # name we got from InfluxDB and use
-                                    # the default type of gauge
-                                    else:
-                                        mname, mtype = (cname, GAUGE)
-
-                                    self.log.debug("values %s, cval %s, cnt %s, "
-                                                   "mname %s, mtype %s, cname %s, "
-                                                   "statuses %s"
-                                                   % (values, cval, cnt, mname,
-                                                      mtype, cname, statuses))
-
-                                    statuses[cname] = (mname, mtype)
-                                    measurements[mname] = cval
-                                cnt += 1
+                self.map_result(d, measurements, statuses, metricmap, whitelist)
 
         for status, metric in statuses.iteritems():
             metric_name, metric_type = metric
@@ -273,3 +220,39 @@ class InfluxDB(services_checks.ServicesCheck):
                     self.rate(metric_name, value, dimensions=dimensions)
                 elif metric_type == GAUGE:
                     self.gauge(metric_name, value, dimensions=dimensions)
+
+    def map_result(self, stat_record, measurements, statuses, metricmap, whitelist):
+        if 'name' in stat_record:
+            name = stat_record['name']
+            if 'server' in name:
+                columns = stat_record['columns']
+                values = stat_record['values'][0]
+
+                # Just make sure we have the same amount of
+                # data in both lists
+                if len(columns) == len(values):
+                    cnt = 0
+                    while cnt < len(columns):
+                        cname = columns[cnt]
+                        cval = values[cnt]
+                        # See if we need to include the metric
+                        if cname in whitelist:
+                            # See if we need to map it to the
+                            # preferred name
+                            if cname in metricmap:
+                                mname, mtype = tuple(metricmap[cname])
+                            # Otherwise just stuff in the original
+                            # name we got from InfluxDB and use
+                            # the default type of gauge
+                            else:
+                                mname, mtype = (cname, GAUGE)
+
+                            self.log.debug("values %s, cval %s, cnt %s, "
+                                           "mname %s, mtype %s, cname %s, "
+                                           "statuses %s"
+                                           % (values, cval, cnt, mname,
+                                              mtype, cname, statuses))
+
+                            statuses[cname] = (mname, mtype)
+                            measurements[mname] = cval
+                        cnt += 1
