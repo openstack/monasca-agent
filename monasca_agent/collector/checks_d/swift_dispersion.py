7f7f21fce61b04b7436ff574500e7dbfbdcaa70a
Falk Reimann <falk.reimann@sap.com>
2015-12-23 16:39:13 +0100

    Swift Dispersion Monasca Agent Plugin
    - sends results of swift-dispersion-report to monasca
    - includes detection
    - includes sample yaml

---
+++ monasca_agent/collector/checks_d/swift_dispersion.py
@@ -0,0 +1,51 @@
+import json
+import logging
+import re
+import subprocess
+import types
+
+import monasca_agent.collector.checks as checks
+
+log = logging.getLogger(__name__)
+
+
+class SwiftDispersion(checks.AgentCheck):
+
+    GAUGES = [
+        "object.copies_expected",
+        "object.copies_found",
+        "object.overlapping",
+        "container.copies_expected",
+        "container.copies_found",
+        "container.overlapping",
+        ]
+
+    def swift_dispersion(self):
+        executable = 'swift-dispersion-report'
+        if 'swift_dispersion' in self.init_config:
+            executable = self.init_config['swift_dispersion']
+
+        cmd = " ".join((executable, '-j'))
+        pipe = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
+                                universal_newlines=True)
+        out = "".join(pipe.stdout.readlines())
+        return out
+
+    def check(self, instance):
+        dimensions = self._set_dimensions(None, instance)
+
+        swift_dispersion = self.swift_dispersion()
+        assert(swift_dispersion)
+
+        dispersion = json.loads(swift_dispersion)
+        for metric in self.GAUGES:
+            log.debug("Checking metric {0}".format(metric))
+            disp_metric = metric.split('.', 1)
+
+            value = dispersion[disp_metric[0]][disp_metric[1]];
+            assert(type(value) in (types.IntType, types.LongType,
+                                   types.FloatType))
+
+            metric = self.normalize(metric.lower(), 'swift.dispersion')
+            log.debug("Sending {0}={1}".format(metric, value))
+            self.gauge(metric, value, dimensions=dimensions)
---
+++ monasca_setup/detection/plugins/swift_dispersion.py
@@ -0,0 +1,28 @@
+import logging
+
+import monasca_setup.agent_config
+import monasca_setup.detection
+
+log = logging.getLogger(__name__)
+
+
+class SwiftDispersion(monasca_setup.detection.Plugin):
+
+    """Detect Swift proxy daemon and setup configuration for swift-dispersion.
+
+    """
+
+    def _detect(self):
+        """Run detection, set self.available True if the service is detected.
+        """
+        if monasca_setup.detection.find_process_cmdline('swift-proxy-server') is not None:
+            self.available = True
+
+    def build_config(self):
+        """Build the config as a Plugins object and return.
+        """
+        config = monasca_setup.agent_config.Plugins()
+        log.info("\tEnabling the swift-dispersion plugin")
+        config['swift_dispersion'] = {'init_config': None, 'instances': [{'name': 'swift-recon'}]}
+
+        return config
