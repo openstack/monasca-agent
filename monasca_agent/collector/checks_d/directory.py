e9c91953baa016c130c24cb7e994941a27fb07f9
Kaiyan Sheng <kaiyan.sheng@hp.com>
2016-03-22 13:52:15 -0600

    Reduce Error Messages from Directory Check
    For the directory that does not exist and
    the sub directories with no permission to
    access, only log the errors once after restart
    the agent.
    
    Change-Id: I7aac1b1e2eaa844f071af8e6a7dc801056d34ed5

--- monasca_agent/collector/checks_d/directory.py
+++ monasca_agent/collector/checks_d/directory.py
@@ -14,6 +14,10 @@ from os import X_OK
 import logging
 
 log = logging.getLogger(__name__)
+missing_dir_instance = set()
+non_exist_dir = set()
+no_access_sub_dir = set()
+no_stat_file = set()
 
 
 class DirectoryCheck(AgentCheck):
@@ -28,39 +32,43 @@ class DirectoryCheck(AgentCheck):
     """
 
     def check(self, instance):
+        global missing_dir_instance
+        global non_exist_dir
         if "directory" not in instance:
-            error_message = 'DirectoryCheck: missing "directory" in config'
-            log.error(error_message)
-            raise Exception(error_message)
+            if instance['dimensions']['service'] not in missing_dir_instance:
+                missing_dir_instance.add(instance['dimensions']['service'])
+                log.error('DirectoryCheck: missing "directory" in config')
+        else:
+            directory = instance["directory"]
+            abs_directory = abspath(directory)
+            if not exists(abs_directory) and abs_directory not in non_exist_dir:
+                non_exist_dir.add(abs_directory)
+                log.error("DirectoryCheck: the directory (%s) does not exist" % abs_directory)
 
-        directory = instance["directory"]
-        abs_directory = abspath(directory)
-
-        if not exists(abs_directory):
-            error_message = "DirectoryCheck: the directory (%s) does not " \
-                            "exist" % abs_directory
-            log.error(error_message)
-            raise Exception(error_message)
-
-        dimensions = self._set_dimensions({"path": directory}, instance)
-        self._get_stats(abs_directory, dimensions)
+            dimensions = self._set_dimensions({"path": directory}, instance)
+            self._get_stats(abs_directory, dimensions)
 
     def _get_stats(self, directory_name, dimensions):
+        global no_access_sub_dir
+        global no_stat_file
         directory_bytes = 0
         directory_files = 0
         for root, dirs, files in walk(directory_name):
             for directory in dirs:
                 directory_root = join(root, directory)
-                if not access(directory_root, X_OK):
-                    log.warn("DirectoryCheck: could not access directory {}".
-                             format(directory_root))
+                if not access(directory_root, X_OK) and directory_root not in no_access_sub_dir:
+                    no_access_sub_dir.add(directory_root)
+                    log.warn("DirectoryCheck: could not access sub directory "
+                             "{}".format(directory_root))
             for filename in files:
                 filename = join(root, filename)
                 try:
                     file_stat = stat(filename)
                 except OSError as ose:
-                    log.warn("DirectoryCheck: could not stat file %s - %s" %
-                             (filename, ose))
+                    if filename not in no_stat_file:
+                        no_stat_file.add(filename)
+                        log.warn("DirectoryCheck: could not stat file %s - "
+                                 "%s" % (filename, ose))
                 else:
                     directory_files += 1
                     directory_bytes += file_stat.st_size
