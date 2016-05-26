a4b105d670ca90019f8fb05cddccb8a936dbc15d
Joachim Barheine <joachim.barheine@sap.com>
2016-05-26 18:42:42 +0200

    fix wrong warning when skipping plugins

--- monasca_setup/utils.py
+++ monasca_setup/utils.py
@@ -48,6 +48,7 @@ def discover_plugins(custom_path):
 def select_plugins(plugin_names, plugin_list, skip=False):
     """:param plugin_names: A list of names
        :param plugin_list: A list of detection plugins classes
+       :param skip: Inverts the behaviour by selecting only those plugins not in plugin_names
        :return: Returns a list of plugins from plugin_list that match plugin_names
     """
     lower_plugins = [p.lower() for p in plugin_names]
@@ -57,7 +58,7 @@ def select_plugins(plugin_names, plugin_list, skip=False):
         if b != skip:
             plugins.append(plugin)
 
-    if len(plugins) != len(plugin_names):
+    if len(plugins) != len(plugin_names) and not skip:
         pnames = [p.__name__ for p in plugin_list]
         log.warn("Not all plugins found, discovered plugins {0}\nAvailable plugins{1}".format(plugins,
                                                                                               pnames))
