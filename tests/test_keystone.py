0556ea6888d55c2308d80e78d0b578f5169772e1
Pablo Rodriguez <pablo.rodriguezarchilla@telefonica.com>
2016-09-21 15:44:39 +0200

    Add service_type, endpoint_type & region_name querying Keystone Catalog
    Monasca URL is pulled from Keystone Service Catalog when not given
    explicitly in agent.yaml, but only the URL with service_type=monitoring
    and endpoint_type=publicURL will be retrieved.
    
    These two options together with the region_name have been included in
    the agent.yaml template and may be configured by monasca-setup, so that
    the KSClient object is initialized with such keywords. This client will
    retrieve the URL from Catalog accordingly.
    
    Change-Id: Icfbfa7c46db648e3d3c27f8e5c72d02957c778f1
    Closes-Bug: #1625573

--- tests/test_keystone.py
+++ tests/test_keystone.py
@@ -1,9 +1,15 @@
 import unittest
+import mock
 
 from monasca_agent.common.keystone import Keystone
+from tests.common import base_config
 
 
 class TestKeystone(unittest.TestCase):
+
+    default_endpoint_type = 'publicURL'
+    default_service_type = 'monitoring'
+
     def testKeyStoneIsSingleton(self):
         keystone_1 = Keystone({})
         keystone_2 = Keystone({})
@@ -11,3 +17,92 @@ class TestKeystone(unittest.TestCase):
 
         self.assertTrue(keystone_1 is keystone_2)
         self.assertTrue(keystone_1 is keystone_3)
+
+    def testServiceCatalogMonascaUrlUsingDefaults(self):
+        Keystone.instance = None
+        with mock.patch('keystoneclient.v3.client.Client') as client:
+            config = base_config.get_config('Api')
+            keystone = Keystone(config)
+            keystone.get_monasca_url()
+            self.assertTrue(client.called)
+            self.assertIn('auth_url', client.call_args[client.call_count])
+            self.assertNotIn('service_type', client.call_args[client.call_count])
+            self.assertNotIn('endpoint_type', client.call_args[client.call_count])
+            self.assertNotIn('region_name', client.call_args[client.call_count])
+            client.return_value.service_catalog.url_for.assert_has_calls([
+                mock.call(endpoint_type=self.default_endpoint_type, service_type=self.default_service_type)
+            ])
+
+    def testServiceCatalogMonascaUrlWithCustomServiceType(self):
+        Keystone.instance = None
+        service_type = 'my_service_type'
+        with mock.patch('keystoneclient.v3.client.Client') as client:
+            config = base_config.get_config('Api')
+            config.update({'service_type': service_type})
+            keystone = Keystone(config)
+            keystone.get_monasca_url()
+            self.assertTrue(client.called)
+            self.assertIn('auth_url', client.call_args[client.call_count])
+            self.assertNotIn('service_type', client.call_args[client.call_count])
+            self.assertNotIn('endpoint_type', client.call_args[client.call_count])
+            self.assertNotIn('region_name', client.call_args[client.call_count])
+            client.return_value.service_catalog.url_for.assert_has_calls([
+                mock.call(endpoint_type=self.default_endpoint_type, service_type=service_type)
+            ])
+
+    def testServiceCatalogMonascaUrlWithCustomEndpointType(self):
+        Keystone.instance = None
+        endpoint_type = 'internalURL'
+        with mock.patch('keystoneclient.v3.client.Client') as client:
+            config = base_config.get_config('Api')
+            config.update({'endpoint_type': endpoint_type})
+            keystone = Keystone(config)
+            keystone.get_monasca_url()
+            self.assertTrue(client.called)
+            self.assertIn('auth_url', client.call_args[client.call_count])
+            self.assertNotIn('service_type', client.call_args[client.call_count])
+            self.assertNotIn('endpoint_type', client.call_args[client.call_count])
+            self.assertNotIn('region_name', client.call_args[client.call_count])
+            client.return_value.service_catalog.url_for.assert_has_calls([
+                mock.call(endpoint_type=endpoint_type, service_type=self.default_service_type)
+            ])
+
+    def testServiceCatalogMonascaUrlWithCustomRegionName(self):
+        Keystone.instance = None
+        region_name = 'my_region'
+        with mock.patch('keystoneclient.v3.client.Client') as client:
+            config = base_config.get_config('Api')
+            config.update({'region_name': region_name})
+            keystone = Keystone(config)
+            keystone.get_monasca_url()
+            self.assertTrue(client.called)
+            self.assertIn('auth_url', client.call_args[client.call_count])
+            self.assertNotIn('service_type', client.call_args[client.call_count])
+            self.assertNotIn('endpoint_type', client.call_args[client.call_count])
+            self.assertNotIn('region_name', client.call_args[client.call_count])
+            client.return_value.service_catalog.url_for.assert_has_calls([
+                mock.call(endpoint_type=self.default_endpoint_type, service_type=self.default_service_type,
+                          attr='region', filter_value=region_name)
+            ])
+
+    def testServiceCatalogMonascaUrlWithThreeCustomParameters(self):
+        Keystone.instance = None
+        endpoint_type = 'internalURL'
+        service_type = 'my_service_type'
+        region_name = 'my_region'
+        with mock.patch('keystoneclient.v3.client.Client') as client:
+            config = base_config.get_config('Api')
+            config.update({'endpoint_type': endpoint_type})
+            config.update({'service_type': service_type})
+            config.update({'region_name': region_name})
+            keystone = Keystone(config)
+            keystone.get_monasca_url()
+            self.assertTrue(client.called)
+            self.assertIn('auth_url', client.call_args[client.call_count])
+            self.assertNotIn('service_type', client.call_args[client.call_count])
+            self.assertNotIn('endpoint_type', client.call_args[client.call_count])
+            self.assertNotIn('region_name', client.call_args[client.call_count])
+            client.return_value.service_catalog.url_for.assert_has_calls([
+                mock.call(endpoint_type=endpoint_type, service_type=service_type,
+                          attr='region', filter_value=region_name)
+            ])
