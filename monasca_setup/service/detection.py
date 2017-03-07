4b8bdb9284bc877bc4f53358a22c6b2fc8fb5172
Dobroslaw Zybort <dobroslaw.zybort@ts.fujitsu.com>
2017-03-07 11:38:37 +0100

    Add SLES to supported distributions
    Change-Id: I251f09006ecc1a3961b3f67737e20d3dd8d405b3
    (cherry picked from commit 09b74b466b592135d8ca1ad0a0c098b00da79a85)

--- monasca_setup/service/detection.py
+++ monasca_setup/service/detection.py
@@ -17,7 +17,11 @@ def detect_init(*args, **kwargs):
     """
     detected_os = platform.system()
     if detected_os == 'Linux':
-        supported_linux_flavors = ['ubuntu', 'debian', 'centos linux', 'red hat enterprise linux server']
+        supported_linux_flavors = [
+            'ubuntu', 'debian',
+            'centos linux', 'red hat enterprise linux server',
+            'suse linux enterprise server'
+        ]
         flavor = platform.linux_distribution()[0]
         if flavor.lower() not in supported_linux_flavors:
             log.warn('{0} is not a support Linux distribution'.format(flavor))
