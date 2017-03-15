1a9a9b51f80d372cae9bfc0f7c72046cfd467d75
Ramon Melero <rmelero@mirantis.com>
2017-03-15 00:06:40 -0500

    Libvirt detection fails if no --config-file flag given. Adds a check for default location of nova.conf.
    Change-Id: Ib3dc14816b1531f3c0f1fab5b841fa5df7d54999

--- monasca_setup/detection/plugins/libvirt.py
+++ monasca_setup/detection/plugins/libvirt.py
@@ -69,6 +69,8 @@ class Libvirt(Plugin):
                 if len(cmd) > 2 and 'python' in cmd[0] and 'nova-compute' in cmd[1]:
                     conf_indexes = [cmd.index(y) for y in cmd if 'nova.conf' in y]
                     if not conf_indexes:
+                        if os.path.exists('/etc/nova/nova.conf'):
+                            nova_conf = "/etc/nova/nova.conf"
                         continue
                     param = conf_indexes[0]
                     if '=' in cmd[param]:
